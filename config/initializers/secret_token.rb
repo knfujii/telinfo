93ecf1d1b3407f536ae16bd36682e1d196c0d597
knfujii <fujiikuninori@facebook.com>
2012-12-18 23:42:58 +0900

    Initial commit
    $ be rails new . -T --skip-bundle
    $ bundle update

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telinfo::Application.config.secret_token = '143e4e3f3f04c4eaff6a2b4cbf5d240bb7c131d073d2abba084ba89d2ae4054615d4f30d2b58b5b3b1977f1e706bff784dd2f87923733d3b2c1697ba135b4cdb'
---
+++ telinfo/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Telinfo::Application.config.secret_token = '091a4e48f0081b64278af30c3392aff185ae0d7edf13b00c4b0938d8dbadb277c26c90e692817757f3ff98876ab6e813a374008844691426e065ecf83ca1aa5d'
